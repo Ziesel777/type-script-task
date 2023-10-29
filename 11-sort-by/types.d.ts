ba980d7d6cd90ef412083d2c558ec23f8430c55c
Ziesel777 <ziesel777@gmail.com>
2023-10-29 22:12:17 +0500

    11-sort-by - правки

--- 11-sort-by/types.d.ts
+++ 11-sort-by/types.d.ts
@@ -1,10 +1,3 @@
-type TObject = {
-	[key:string]: number|string|TObject,
-};
-
 declare module 'sort-by' {
-	export default function sortBy(...args: string[]): (
-			obj1: TObject,
-			obj2: TObject
-		)=>number;
+	export default function sortBy<T>(...args:  ReadonlyArray<string>): (a: T, b: T )=>number;
 }
